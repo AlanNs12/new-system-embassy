1d1c567357bf4e06bc232ca0b6d3c67cfb0797bf
AlanNs12 <92896200+AlanNs12@users.noreply.github.com>
2025-07-28 16:00:14 -0300

    atualizações archs

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,47 +4,49 @@ const jwt = require('jsonwebtoken');
 
 module.exports = {
     //cadastro de usuarios
-    async register(req, res){
-        try{
-            const {nome, senha, tipo} = req.body;
+    async register(req, res) {
+        try {
+            const { nome, senha, tipo } = req.body;
             if (!nome || !senha) {
-                return res.status(400).json({message: 'Nome e senha são obrigatorios'});
+                return res.status(400).json({ message: 'Nome e senha são obrigatorios' });
             }
             const hash = await bcrypt.hash(senha, 10);
-        const result = await pool.query(
-            'INSERT INTO usuarios (nome, senha, tipo) VALUES ($1, $2,$3) RETURNING id' [nome, hash, tipo || 'user'] 
-        );
-        res.status(201).json({message: 'Usuario Criado', id: result.rows[0].id});
+            const result = await pool.query(
+                'INSERT INTO usuarios (nome, senha, tipo) VALUES ($1, $2, $3) RETURNING id',
+                [nome, hash, tipo || 'user']
+            );
+
+            res.status(201).json({ message: 'Usuario Criado', id: result.rows[0].id });
         } catch (err) {
             console.error(err);
-            res.status(500).json({message: 'Erro ao criar usuario'});
-        }  
+            res.status(500).json({ message: 'Erro ao criar usuario' });
+        }
     },
 
     // login 
     async login(req, res) {
         try {
-            const {nome, senha} = req.body;
+            const { nome, senha } = req.body;
             const result = await pool.query('SELECT * FROM usuarios WHERE nome=$1', [nome]);
 
-            if (result.rows.length === 0){
-                return res.status(401).json({message: 'Usuario ou senha incorretos'});
+            if (result.rows.length === 0) {
+                return res.status(401).json({ message: 'Usuario ou senha incorretos' });
             }
             const user = result.rows[0];
             const isMatch = await bcrypt.compare(senha, user.senha);
             if (!isMatch) {
-                return res.status(401).json({message: 'Senha incorreta'});
+                return res.status(401).json({ message: 'Senha incorreta' });
             }
             const token = jwt.sign(
-                {id: user.id, nome: user.nome, tipo: user.tipo},
+                { id: user.id, nome: user.nome, tipo: user.tipo },
                 process.env.JWT_SECRET,
-                {expiresIn: '12h'}
+                { expiresIn: '12h' }
             );
 
-            res.json({token, tipo: user.tipo});
+            res.json({ token, tipo: user.tipo });
         } catch (err) {
             console.error(err);
-            res.status(500).json({message: 'Erro ao fazer login'});
+            res.status(500).json({ message: 'Erro ao fazer login' });
         }
     },
 };
